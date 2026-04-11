d08568d7574a154c347d8cefbdaca2304d9ae659
wichan <wichan7@naver.com>
2026-04-11 22:12:46 +0900

    feat: 액츄에이터 추가

---
+++ src/main/java/com/prettygoodorg/pluto_api/common/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.prettygoodorg.pluto_api.common.config;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(EndpointRequest.to("health", "info", "livenessState", "readinessState")).permitAll()
+                        .requestMatchers("/actuator/**").denyAll()
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
